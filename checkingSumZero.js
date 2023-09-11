d6b8599a24435de4709d2f1b5c26ce152d8436d8
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-09-11 11:43:03 +0530

    checking sum zero problem solve

---
+++ checkingSumZero.js
@@ -0,0 +1,8 @@
+// let a = [-5, -4, -3, -2, 0, 2, 4, 6, 8]
+
+const ans = a.reduce((a, b) => {
+    return a + b;
+})
+console.log(ans);
+if (ans) console.log("not zero");
+else console.log("zero");
